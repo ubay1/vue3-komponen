0fd5b41379b439dda05ee34a24c657e7385b19d3
maung-dot-js <asanuri37@gmail.com>
2023-10-14 03:48:50 +0700

    test-push

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ const isOpenDialog = ref<boolean>(false);
 
 <template>
   <div class="h-full px-4 my-4">
-    <div class="grid grid-cols-2">
+    <div class="grid grid-cols-2 gap-10 lt-md:grid-cols-1 lt-md:gap-2">
       <div class="">
         <div class="font-bold text-xl">Alert</div>
         <AlertError message="Alert error" />
@@ -19,7 +19,7 @@ const isOpenDialog = ref<boolean>(false);
       </div>
       <div>
         <div class="font-bold text-xl">Dialog</div>
-        <button @click="isOpenDialog = true">Open dialog</button>
+        <button class="btn-fill-primary mt-4" @click="isOpenDialog = true">Open dialogs</button>
         <Teleport to="body">
           <Modal v-if="isOpenDialog" title="Test Dialog" @close="isOpenDialog = false"> ini dialog modal </Modal>
         </Teleport>
