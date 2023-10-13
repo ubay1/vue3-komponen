6311303404fb3a6861bac6f64b450c7331bbdda1
maung-dot-js <146605833+maung-dot-js@users.noreply.github.com>
2023-10-13 15:35:10 +0700

    show komponen alert error

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Error from "./components/alerts/Error.vue";
+</script>
 
 <template>
-  <div class="h-full">
-    <div></div>
+  <div class="h-full px-4 my-4">
+    <div class="grid grid-cols-2">
+      <div class="">
+        <div class="font-bold text-xl">Alert</div>
+        <Error message="Alert error" />
+      </div>
+    </div>
   </div>
 </template>
